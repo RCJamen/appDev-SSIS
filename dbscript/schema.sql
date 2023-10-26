0dbda2cbcb75f98de00644ed8ca1c2aaeef98434
RCJamen <rcjamen10@gmail.com>
2023-10-26 15:37:12 +0800

    fix: Updated SQL Schema with Data

--- dbscript/schema.sql
+++ dbscript/schema.sql
@@ -32,10 +32,20 @@ FOREIGN KEY(coursecode) REFERENCES courses(code)
 
 INSERT INTO `colleges`(`code`, `name`)
 VALUES ('CCS', 'College of Computer Studies'),
-	('CSM', 'College of Science and Mathematics');
+	('CSM', 'College of Science and Mathematics'),
+	('COET', 'College of Engineering and Technology'),
+	('CASS', 'College of Arts and Social Sciences'),
+	('CEBA', 'College of Economics and Business Administration');
 	
 INSERT INTO `courses`(`code`, `name`, `collegecode`)
 VALUES ('BSCS', 'Bachelor of Science in Computer Science', 'CCS'),
-       ('BSIS', 'Bachelor of Science in Information System', 'CCS');
+		('BSA', 'Bachelor of Science in Accountancy', 'CEBA'),
+		('BSM', 'Bachelor of Science in Mathematics', 'CSM'),
+		('BSCE', 'Bachelor of Science in Civil Engineering', 'COET'),
+		('BSP', 'Bachelor of Science in Philosophy', 'CASS');
 
-INSERT INTO `students`(`id`, `firstname`, `lastname`,`coursecode`,`year`,`gender`) VALUES ('2019-2093','Ramel Cary','Jamen','BSCS','1','Male');
+INSERT INTO `students`(`id`, `firstname`, `lastname`,`coursecode`,`year`,`gender`) 
+VALUES ('2019-2093','Ramel Cary','Jamen','BSCS','1','Male'),
+		('2019-0523','Edward Vincent','Escasio','BSCE','4','Male'),
+		('2018-5069','Edward James','Bongo','BSA','3','Male'),
+		('2019-2932','Kenneth Martin','Gagno','BSM','4','Male');

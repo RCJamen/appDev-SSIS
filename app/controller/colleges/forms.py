37dc18301b39e5baaaa7ec3a464d0aaadf666e42
RCJamen <rcjamen10@gmail.com>
2023-10-27 07:28:20 +0800

    fix: college forms char-only

--- app/controller/colleges/forms.py
+++ app/controller/colleges/forms.py
@@ -1,8 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, SubmitField, SelectField
-import app.models.college as models
+from wtforms import StringField, validators, SubmitField
+
 
 class collegeForm(FlaskForm):
-    code = StringField("Code", [validators.DataRequired()])
-    name = StringField("Name", [validators.DataRequired()])
+    code = StringField(
+        "Code",
+        [
+            validators.DataRequired(),
+            validators.Regexp(
+                regex=r"^[A-Za-z]+$",
+                message="Only letters are allowed in the code field.",
+            ),
+        ],
+    )
+    name = StringField(
+        "Name",
+        [
+            validators.DataRequired(),
+            validators.Regexp(
+                regex=r"^[A-Za-z]+$",
+                message="Only letters are allowed in the name field.",
+            ),
+        ],
+    )
     submit = SubmitField("Submit")
